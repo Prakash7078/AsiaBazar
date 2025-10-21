69cfb92db9837afc7a581ec3c8db6bc7b5b50d00
Prakash7078 <20bq1a05j5@vvit.net>
2025-10-21 17:39:18 -0500

    getcartitems api issue fixed

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -43,8 +43,8 @@ function Navbar() {
     setIsMenuOpen(!isMenuOpen);
   };
   useEffect(() => {
-    dispatch(getCartItems({user_id:userInfo?.user_id}));
-  },[userInfo?.user_id]);
+    dispatch(getCartItems({user_id:userInfo?._id}));
+  },[userInfo,dispatch]);
   const handleSignout = async () => {
     await dispatch(logoutUser());
     navigate("/");
