5c9972731fb838bf450d0f8b5a8ffd2b30be5d5e
Prakash7078 <20bq1a05j5@vvit.net>
2025-12-13 05:23:50 +0530

    changes uuidv4  made in middleware file

--- server/middleware/uploadMiddleware.js
+++ server/middleware/uploadMiddleware.js
@@ -1,6 +1,6 @@
 const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
 const express=require('express');
-const { v4: uuidv4 } = require('uuid');
+import { v4 as uuidv4 } from 'uuid';
 
 
 const dotenv=require('dotenv');
