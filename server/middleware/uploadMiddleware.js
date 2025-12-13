0c2bb373fe11af031c0f5d80591c2bd1ac0b85fa
Prakash7078 <20bq1a05j5@vvit.net>
2025-12-13 05:31:01 +0530

    Fixed the uuid error

--- server/middleware/uploadMiddleware.js
+++ server/middleware/uploadMiddleware.js
@@ -1,74 +1,51 @@
 const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
-const express=require('express');
-import { v4 as uuidv4 } from 'uuid';
+const express = require('express');
+const dotenv = require('dotenv');
 
-
-const dotenv=require('dotenv');
 dotenv.config();
+
 const s3Client = new S3Client({
-    region: process.env.AWS_ACCOUNT_REGION,
-    credentials: {
-      accessKeyId: process.env.AWS_ACCOUNT_ACCESS_KEY,
-      secretAccessKey: process.env.AWS_ACCOUNT_SECRET_ACCESS_KEY,
-    },
-  });
+  region: process.env.AWS_ACCOUNT_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCOUNT_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_ACCOUNT_SECRET_ACCESS_KEY,
+  },
+});
 
-const uploadImage=async (folderName,file) => {
-  console.log("Uploading file:", file.originalname);
-    try {
-      const contentType = file.mimetype;
-      const key = `${folderName}/${uuidv4()}_${file.originalname}`;
+// cache the ESM import so we only do it once
+let uuidModulePromise = null;
 
-      const command =new PutObjectCommand({
-        Bucket: process.env.AWS_BUCKET_NAME,
-        Key:key,
-        Body: file.buffer,
-        ContentType: contentType,
-      });
-  
-      const response = await s3Client.send(command);
-      const url = `https://${process.env.AWS_BUCKET_NAME}.s3.amazonaws.com/${key}`;
-      return { response, key, url };    
-    } catch (error) {
-      console.error("Failed to upload file to S3:", error);
-    }
-  };
-  
-module.exports=uploadImage;
+async function getUuidV4() {
+  if (!uuidModulePromise) {
+    uuidModulePromise = import('uuid');
+  }
+  const mod = await uuidModulePromise;
+  return mod.v4;
+}
 
-//sending sns messages
-// const express = require('express');
-// const app = express();
-// require('dotenv').config();
+const uploadImage = async (folderName, file) => {
+  console.log('Uploading file:', file.originalname);
+  try {
+    const uuidv4 = await getUuidV4();
 
-// var AWS = require('aws-sdk')
+    const contentType = file.mimetype;
+    const key = `${folderName}/${uuidv4()}_${file.originalname}`;
 
-// app.get('/', (req, res) => {
+    const command = new PutObjectCommand({
+      Bucket: process.env.AWS_BUCKET_NAME,
+      Key: key,
+      Body: file.buffer,
+      ContentType: contentType,
+    });
 
-//     console.log("Message = " + req.query.message);
-//     console.log("Number = " + req.query.number);
-//     console.log("Subject = " + req.query.subject);
-//     var params = {
-//         Message: req.query.message,
-//         PhoneNumber: '+' + req.query.number,
-//         MessageAttributes: {
-//             'AWS.SNS.SMS.SenderID': {
-//                 'DataType': 'String',
-//                 'StringValue': req.query.subject
-//             }
-//         }
-//     };
+    const response = await s3Client.send(command);
+    const url = `https://${process.env.AWS_BUCKET_NAME}.s3.amazonaws.com/${key}`;
 
-//     var publishTextPromise = new AWS.SNS({ apiVersion: '2010-03-31' }).publish(params).promise();
+    return { response, key, url };
+  } catch (error) {
+    console.error('Failed to upload file to S3:', error);
+    throw error; // rethrow so caller can handle it
+  }
+};
 
-//     publishTextPromise.then(
-//         function (data) {
-//             res.end(JSON.stringify({ MessageID: data.MessageId }));
-//         }).catch(
-//             function (err) {
-//                 res.end(JSON.stringify({ Error: err }));
-//             });
-
-// });
-
-// app.listen(3000, () => console.log('SMS Service Listening on PORT 3000'))
+module.exports = uploadImage;
