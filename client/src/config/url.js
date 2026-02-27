376295bcf652ff0feafa3dbbbc7b177fb6dc4970
Prakash7078 <20bq1a05j5@vvit.net>
2026-02-27 17:32:42 -0600

    change backend url

--- client/src/config/url.js
+++ client/src/config/url.js
@@ -1,2 +1,2 @@
-// export const BASE_URL = "https://asia-bazar-api.vercel.app";
-export const BASE_URL = "http://localhost:5001";
+export const BASE_URL = "https://asia-bazar-api.vercel.app";
+// export const BASE_URL = "http://localhost:5001";
